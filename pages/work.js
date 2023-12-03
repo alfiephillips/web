832ecf94c3bf1274a7d4427e393925eb642c6250
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2023-12-03 23:23:35 +0000

    bug: eslint brought up some inconsistencies

--- pages/work.js
+++ pages/work.js
@@ -1,22 +1,22 @@
 import {
-    PageContainer, Navbar, NavLink, 
-    Title, Highlight, Description, FooterText} 
-    from "../components";
+  PageContainer, Navbar, NavLink, 
+  Title, Description} 
+  from "../components";
 
-import React, {useState} from "react"
+import React from "react"
 
 const WorkPage = () => {
-    return (
-        <PageContainer>
-            <Navbar>
-            <NavLink href="/">home</NavLink>
-            <NavLink href="" $activeColor="green">work</NavLink>
-            <NavLink href="/contact">contact</NavLink>
-            </Navbar>
-            <Title>my work 💻</Title>
-            <Description>working on this page...</Description>
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      <Navbar>
+        <NavLink href="/">home</NavLink>
+        <NavLink href="" $activeColor="green">work</NavLink>
+        <NavLink href="/contact">contact</NavLink>
+      </Navbar>
+      <Title>my work 💻</Title>
+      <Description>working on this page...</Description>
+    </PageContainer>
+  )
 }
 
 export default WorkPage;
